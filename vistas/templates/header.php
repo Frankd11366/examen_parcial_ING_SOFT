93034a934be3738ae9c706cac1e4316f15f0b5c8
Frankd11366 <capurivas@gmail.com>
2024-05-20 10:51:21 -0600

    header

--- vistas/templates/header.php
+++ vistas/templates/header.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="shortcut icon" href="/images/iconocli.png" type="image/x-icon">
-    <title>VISITANTES</title>
+    <title>VISITANTES COMANDO</title>
 </head>
 <body>
 <?php include_once '../../vistas/templates/navbar.php'?>
